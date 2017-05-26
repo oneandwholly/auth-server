8a410625f2e308793f88bf95a98e7928e60f1eda
Jon Huh <oneandwholly@gmail.com>
2017-05-26 18:32:16 +0900

    set up server

---
+++ index.js
@@ -0,0 +1,18 @@
+// Main starting point of the application
+
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }))
+
+
+// Server Setup
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
