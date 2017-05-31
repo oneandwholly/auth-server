1422742ac43f39b924d3d22056d46dca199f1527
Jon Huh <oneandwholly@gmail.com>
2017-05-31 15:53:23 +0900

    define comparePassword method on userSchema

--- services/passport.js
+++ services/passport.js
@@ -8,7 +8,16 @@ const LocalStrategy = require('passport-local');
 // Create local strategy
 const localOptions = { usernameField: 'email' };
 const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
 
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+      return done(null, user);
+    });
+  });
 });
 
 // set options for JWT strategy
